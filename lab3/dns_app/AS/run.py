79585c92fd1bca4016bcf125125233f37fa60186
Victor Guillemot <vgmt@Victors-MBP.lan>
2022-05-18 14:35:02 -0400

    edited run files – didn't seem to work when uploading on github.

--- lab3/dns_app/AS/run.py
+++ lab3/dns_app/AS/run.py
@@ -1,32 +1,56 @@
+# Imports
+
+from flask import Flask
+from flask import request
+from flask import Response
+from time import gmtime, strftime
 import json
-import socket
-import sys
+import os
+import requests
+
+app = Flask(__name__)
 
 
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(("127.0.0.1", 53533))
-    while True:
-        try:
-            client_data, client_address = server_socket.recvfrom(1024)
-            print ('Got connection from', client_address, client_data)
-            client_data = client_data.decode()
-            print("client_data: ", client_data, type(client_data))
-        except socket.error as e:
-            print(e)
-            response = {
-                "message": "Bad Request",
-                "status_code": 400
-            }
-            return 400
-        with open("dns.json", "w+") as dns_file:
-            dns_file.write(client_data)
-        server_socket.close()
-        break
+@app.route('/home')
+def AS_route():
+    return "AS HOME"
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Shutting Down DNS Server')
-        sys.exit(0)
+
+@app.route('/', methods=['GET', 'PUT'])
+def AS():
+
+    #setting up the file:
+
+    file = 'rcrd_dns.json'
+    if not os.path.exists(file):
+        os.system(r'touch rcrd_dns.json')
+        file = 'rcrd_dns.json'
+
+    # loading the data into rcrd_dns
+
+    if request.method == 'GET':
+        key = request.args.get('name')
+        with open(file, 'r') as json_file:
+            data = json.load(json_file)
+            if key not in data:
+                return Response("ERROR: hostname not found", status=404)
+            else:
+                address = data.get(key)
+                return Response(address, status=200)
+
+    else:
+        data_get = request.form
+        host_name = data_get['name']
+        ip_address = data_get['address']
+        dict = {}
+        dict[host_name] = ip_address
+        with open(file, 'w') as json_file:
+            json.dump(dict, json_file)
+        return Response("registered!", status=200)
+
+
+# App running on port 5353
+
+app.run(host='0.0.0.0',
+        port=53533,
+        debug=True)
--- lab3/dns_app/FS/run.py
+++ lab3/dns_app/FS/run.py
@@ -1,70 +1,45 @@
-# Imports
-
-from flask import Flask
-from flask import request
-from flask import Response
+from flask import Flask, request, Response
+import requests
 import json
-import socket
+
+
 app = Flask(__name__)
 
-#register route – 
 
-@app.route('/register', methods = ["PUT"])
+@app.route('/')
+def FS_route():
+    return 'Welcome to FS'
+
+# UDP to Server
+@app.route('/register')
 def register():
-    content = request.get_json()
-    hostname = content.get('hostname')
-    ip = content.get('ip')
-    as_ip = content.get('as_ip')
-    as_port = int(content.get('as_port'))
-    data = {
-            "TYPE": "A",
-            "NAME": hostname,
-            "VALUE": ip,
-            "TTL": 10
-        }
-    data = json.dumps(data)
-    print("data: ", data)
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        client_socket.sendto(data.encode(), ('127.0.0.1', as_port))
-        client_socket.close()
-    except socket.error as e:
-        print(e)
-        response = {
-            "message": "Bad Request",
-            "status_code": 400
-        }
-        return Response(response=json.dumps(response), status=400, mimetype='application/json')
-    response = {
-        "status_code": 201,
-        "hostname": hostname,
-        "ip": ip,
-        "as_ip": as_ip,
-        "as_port": as_port
-        }
-    return Response(response=json.dumps(response), status=200, mimetype='application/json')
+    host_name = request.args.get('hostname')
+    ip_address = '0.0.0.0'
+    dict = {}
+    dict['name'] = host_name
+    dict['address'] = ip_address
+    req = requests.put('http://0.0.0.0:53533', data = dict)
+    return Response(req.text, status = 201)
 
-@app.route('/fibonacci', methods = ["GET"])
-def fibonacci():
-    number = request.args.get('number', None)
-    if number is None or number.isdigit() == False:
-        response = {
-            "message": "Bad Request",
-            "status_code": 400
-        }
-        return Response(response=json.dumps(response), status=400, mimetype='application/json')
-    number = int(number)
-    data = [0, 1]
-    if number > 2:
-        for i in range (2, number):
-            data.append(data[i-1] + data[i-2])
-    response = {
-        "status_code": 200,
-        "result": data[number-1]
-    }
-    return Response(response=json.dumps(response), status=200, mimetype='application/json')
+@app.route('/fibonacci')
+def fabonacci():
+    x_fibo = int(request.args.get('number'))
+    if type(x_fibo) != int:
+        return Response("ERROR: please input a number.", status = 400)
+    result = fib(x_fibo)
+    return Response("the fibo for {0} is: {1}".format(x_fibo, result), status = 200)
 
-# App – running on port 9090
+
+
+def fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+# app – running on port 9090
 
 app.run(host='0.0.0.0',
         port=9090,
